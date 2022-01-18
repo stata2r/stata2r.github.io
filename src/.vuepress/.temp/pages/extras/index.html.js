12572086b280cdb549327f74b9f3435632efacc6
Kyle F. Butts <buttskyle96@gmail.com>
2022-01-17 21:56:36 -0700

    Add light theme for code

--- src/.vuepress/.temp/pages/extras/index.html.js
+++ src/.vuepress/.temp/pages/extras/index.html.js
@@ -157,7 +157,20 @@ export const data = {
     }
   ],
   "git": {
-    "updatedTime": 1642398703000
+    "updatedTime": 1642438884000
   },
   "filePathRelative": "extras/README.md"
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
 }
