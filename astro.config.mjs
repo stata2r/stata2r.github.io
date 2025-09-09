ed675f00b16394618016f391b7694490f009f982
Kyle Butts <buttskyle96@gmail.com>
2025-09-08 19:39:39 -0500

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,52 @@
-import { defineConfig } from "astro/config"
-import starlight from "@astrojs/starlight"
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stata2r.github.io/',
+  site: "https://stata2r.github.io/",
   redirects: {
-    '/data.table': '/data_table'
+    "/data.table": "/data_table",
   },
   // outDir: 'docs',
-	integrations: [
-		starlight({
-			title: "stata2R",
-			social: {
-				github: "https://github.com/stata2r/stata2r.github.io/",
-			},
-			customCss: ["./src/styles/main.css"],
-			sidebar: [
-				{
-					label: "Guides",
-					items: [
+  integrations: [
+    starlight({
+      title: "stata2R",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/stata2r/stata2r.github.io/",
+        },
+      ],
+      customCss: ["./src/styles/main.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
             {
-							label: "Intro",
-							link: "/",
-						},
-						{
-							label: "data.table",
-							link: "/data_table/",
-						},
-						{
-							label: "fixest",
-							link: "/fixest/",
-						},
-						{
-							label: "extras",
-							link: "/extras/",
-						},
-					],
-				},
-			],
-		}),
-		tailwind({
+              label: "Intro",
+              link: "/",
+            },
+            {
+              label: "data.table",
+              link: "/data_table/",
+            },
+            {
+              label: "fixest",
+              link: "/fixest/",
+            },
+            {
+              label: "extras",
+              link: "/extras/",
+            },
+          ],
+        },
+      ],
+    }),
+    tailwind({
       applyBaseStyles: false,
     }),
-	],
-})
+  ],
+});
